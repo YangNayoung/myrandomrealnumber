3b1a2fe3e0cf72644e18f808b9988abb72ac2200
Nayoung <s2018s32@e-mirim.hs.kr>
2020-05-19 20:34:58 +0900

    초기 커밋.

---
+++ myrandomrealnumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myrandomrealnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNum = random.Next(1000);
+            Console.WriteLine(randomNum);
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                Double num = Double.Parse(Console.ReadLine());
+                if (num > randomNum)
+                {
+                    Console.WriteLine(num + "보다는 작은 숫자 입니다.");
+                }
+                else if (num < randomNum)
+                {
+                    Console.WriteLine(num + "보다는 큰 숫자 입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+            }
+        }
+    }
+}
